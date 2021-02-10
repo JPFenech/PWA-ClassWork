cee377a0a9ae27cd26d61ba399831b6050855986
Jean <fenechjeanpierre@gmail.com>
2021-02-10 21:58:22 +0100

    ,

--- service-worker.js
+++ service-worker.js
@@ -42,12 +42,12 @@ addEventListener('fetch', function(event) {
                     return res;   // return the fetched data
                   })
               })
-              .catch(function(err) {       // fallback mechanism
-                return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                  .then(function(cache) {
-                    return cache.match('/offline.html');
-                  });
-              });
+            //   .catch(function(err) {       // fallback mechanism
+            //     return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+            //       .then(function(cache) {
+            //         return cache.match('/offline.html');
+            //       });
+            //   });
           }
         })
     );
