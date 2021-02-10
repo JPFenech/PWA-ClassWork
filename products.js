3167f013c5b6aca337ad06603086d19e1b8f6ee0
Jean <fenechjeanpierre@gmail.com>
2021-02-10 21:25:27 +0100

    1

---
+++ products.js
@@ -0,0 +1,74 @@
+let products = [
+    {
+      "id": 1002,
+      "title": "Computer",
+      "description": "Computer Lessons for all ages",
+      "price": 20,
+      "image": "images/computer.jpg",
+      "availableInventory": 5,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Guitar",
+      "description": "learn to play guitar fast and easy!! ",
+      "price": 25,
+      "image": "images/guitar.jpg",
+      "availableInventory": 10,
+      "rating": 4
+    },
+    {
+      "id": 1004,
+      "title": "Gymnastics",
+      "description": "Keep body and mind in perfect shape!",
+      "price": 10.50,
+      "image": "images/gymnastics.jpg",
+      "availableInventory": 5,
+      "rating": 5
+    },
+    {
+      "id": 1005,
+      "title": "Karate",
+      "description": "Learn self defence  - adults only",
+      "price": 12.50,
+      "image": "images/karate.jpg",
+      "availableInventory": 5,
+      "rating": 4
+    },
+    {
+      "id": 1006,
+      "title": "Language",
+      "description": "Learn a different language today!!",
+      "price": 12.50,
+      "image": "images/language.jpg",
+      "availableInventory": 5,
+      "rating": 4
+    },
+    {
+      "id": 1007,
+      "title": "Maths",
+      "description": "Advanced Mathematics",
+      "price": 18.50,
+      "image": "images/maths.jpg",
+      "availableInventory": 7,
+      "rating": 4
+    },
+    {
+      "id": 1008,
+      "title": "Physics",
+      "description": "Interesting experiments done at our Lab!",
+      "price": 29.50,
+      "image": "images/physics.jpg",
+      "availableInventory": 5,
+      "rating": 4
+    },
+    {
+      "id": 1009,
+      "title": "Piano",
+      "description": "Create amazing tunes in just a few weeks!",
+      "price": 29.50,
+      "image": "images/piano.jpg",
+      "availableInventory": 5,
+      "rating": 4
+    }
+  ]
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+var cacheName = 'lessonstore';
+var cacheFiles = [ 
+    'index.html',
+    'products.js',
+    'style.css',
+    'lessons.webmanifest',
+    'images/piano.jpg',
+    'images/physics.jpg',
+    'images/maths.jpg',
+    'images/language.jpg',
+    'images/karate.jpg',
+    'images/gymnastics.jpg',
+    'images/guitar.jpg',
+    'images/computer.jpg',
+    'images/icon512.png',
+    'images/icon32.png'
+
+];
+
+self.addEventListener('install', (e) =>  {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r){
+            return r || fetch(e.request).then (function (response){
+                 return caches.open(cacheName).then(function(cache){
+                     cache.put(e.reuqest, response.clone());
+                     return response;
+                 });
+            });
+        })
+    );
+});
